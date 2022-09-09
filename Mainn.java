e932bfc63b2a4940256358f9542ab9a0e2f57544
Nguyen Chon Loi <chonloi2561998@gamil.com>
2022-09-09 11:07:50 +0700

    variable

---
+++ Mainn.java
@@ -0,0 +1,17 @@
+public class Mainn {
+    public static void main(String[] args) {
+        int i = 12;
+        float f = 220.2f;
+        double d=2.021d;
+        boolean b= true;
+        char c= 'q';
+        String s= "Hà nội";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i = 12;
+        float f = 220.2f;
+        double d=2.021d;
+        boolean b= true;
+        char c= 'q';
+        String s= "Hà nội";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+
+    }
+}
